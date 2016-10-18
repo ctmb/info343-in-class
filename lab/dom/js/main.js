529844d6669e8c1a949204d81dae56ebf827439b
Carson McBride <ctmb@uw.edu>
2016-10-18 15:22:38 -0700

    This is what I did in lab!

--- lab/dom/js/main.js
+++ lab/dom/js/main.js
@@ -6,3 +6,32 @@ var boxOne = document.getElementById('one'),
 	boxFour = document.getElementById('four'),
 	boxFive = document.getElementById('five'),
 	boxSix = document.getElementById('six');
+
+boxOne.addEventListener("click", fadeToBlack);
+function fadeToBlack() {
+	boxOne.classList.add("black");
+	boxOne.classList.add("transition");
+}
+
+boxTwo.addEventListener("click", fall);
+function fall() {
+	boxTwo.classList.add("fall");
+	boxTwo.classList.add("transition");
+}
+
+boxThree.addEventListener("mouseover", fadeToWhite);
+function fadeToWhite() {
+	boxThree.classList.add("white");
+	boxThree.classList.add("transition");
+}
+boxThree.addEventListener("mouseout", fadeToGrey);
+function fadeToGrey() {
+	boxThree.classList.remove("white");
+	boxThree.classList.add("transition");
+}
+
+boxFour.addEventListener("click", fall);
+boxFour.addEventListener("click", float);
+function float() {
+	boxFour.classList.add("float");
+}
